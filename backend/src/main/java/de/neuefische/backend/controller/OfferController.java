80e241227b730c3b497048e4853a4bc2edc9648b
VladimirKlippert93 <vladimir.klippert93@gmail.com>
2022-12-16 10:45:59 +0100

    Add OfferController.java

---
+++ backend/src/main/java/de/neuefische/backend/controller/OfferController.java
@@ -0,0 +1,24 @@
+package de.neuefische.backend.controller;
+
+import de.neuefische.backend.models.Offer;
+import de.neuefische.backend.service.OfferService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/offers")
+public class OfferController {
+    private final OfferService offerService;
+
+    public OfferController(OfferService offerService) {
+        this.offerService = offerService;
+    }
+
+    @GetMapping
+    public List<Offer> getAllOffers(){
+        return offerService.getAllMovies();
+    }
+}
