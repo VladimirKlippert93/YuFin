0520699958083b2176179c9e5a8d681b35f89094
VladimirKlippert93 <vladimir.klippert93@gmail.com>
2022-12-16 11:21:40 +0100

    Update id

--- backend/src/main/java/de/neuefische/backend/models/Offer.java
+++ backend/src/main/java/de/neuefische/backend/models/Offer.java
@@ -4,6 +4,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document("Offer")
 public record Offer(
+        String id,
         String title,
         String price,
         String description
