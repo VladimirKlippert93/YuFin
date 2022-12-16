d546e776a801a467d14ee12f908a0ab843f9286a
VladimirKlippert93 <vladimir.klippert93@gmail.com>
2022-12-16 11:17:44 +0100

    Add OfferCard.tsx -> box for a offer

---
+++ frontend/src/OfferGallery/component/OfferCard.tsx
@@ -0,0 +1,17 @@
+import {Offer} from "../models/Offer";
+
+export type OfferCardProps = {
+    offer: Offer
+}
+
+export default function OfferCard(props: OfferCardProps){
+    return(
+        <div className={"offercard"}>
+            {!props && <p>loading files...</p>}
+            <div className={"offercard_border"}>
+                <h2>{props.offer.title}</h2>
+                <p>{props.offer.price}</p>
+            </div>
+        </div>
+    )
+}
