f8cb41823d7fc0aef8992d66a6b8dbe8eea91207
VladimirKlippert93 <vladimir.klippert93@gmail.com>
2022-12-19 12:37:42 +0100

    Update OfferService & Test with AddressRepo

--- backend/src/main/java/de/neuefische/backend/service/OfferService.java
+++ backend/src/main/java/de/neuefische/backend/service/OfferService.java
@@ -1,6 +1,7 @@
 package de.neuefische.backend.service;
 
 import de.neuefische.backend.models.Offer;
+import de.neuefische.backend.repository.AddressRepo;
 import de.neuefische.backend.repository.OfferRepo;
 import org.springframework.stereotype.Service;
 
@@ -10,8 +11,9 @@ import java.util.List;
 public class OfferService {
     private final OfferRepo offerRepo;
 
-    public OfferService(OfferRepo offerRepo) {
+    public OfferService(OfferRepo offerRepo, AddressRepo addressRepo) {
         this.offerRepo = offerRepo;
+
     }
 
     public List<Offer> getAllOffers(){
