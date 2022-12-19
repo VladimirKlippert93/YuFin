2105b9d137eb2db6a51823b69ce20c94dd9d7b45
VladimirKlippert93 <vladimir.klippert93@gmail.com>
2022-12-19 15:03:24 +0100

    Update Address

--- backend/src/main/java/de/neuefische/backend/models/Address.java
+++ backend/src/main/java/de/neuefische/backend/models/Address.java
@@ -1,9 +1,7 @@
 package de.neuefische.backend.models;
 
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document("Address")
 public record Address(
         @Id
         String id,
