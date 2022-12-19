fa116c13655b6704e51cfe9e11a8ae087f02d8ad
VladimirKlippert93 <vladimir.klippert93@gmail.com>
2022-12-19 11:11:02 +0100

    Update Offer with Address

--- backend/src/main/java/de/neuefische/backend/models/Offer.java
+++ backend/src/main/java/de/neuefische/backend/models/Offer.java
@@ -2,11 +2,14 @@ package de.neuefische.backend.models;
 
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.List;
+
 @Document("Offer")
 public record Offer(
         String id,
         String title,
         String price,
+        List<Address> address,
         String description
 ) {
 
