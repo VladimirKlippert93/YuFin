7231fb54aad0f90ea75d6bb4c101ef6d49190e1a
VladimirKlippert93 <vladimir.klippert93@gmail.com>
2022-12-19 12:34:58 +0100

    Update Address in frontend & backend

--- backend/src/main/java/de/neuefische/backend/models/Address.java
+++ backend/src/main/java/de/neuefische/backend/models/Address.java
@@ -1,14 +1,17 @@
 package de.neuefische.backend.models;
 
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document("Address")
 public record Address(
+        @Id
         String id,
         String street,
         String streetNumber,
         String city,
         int zip,
-        String country
+        String country,
+        String offerId
 ) {
 }
