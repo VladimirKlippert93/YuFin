bc0f940e3c510557354f1129528d40c03e006da9
VladimirKlippert93 <vladimir.klippert93@gmail.com>
2022-12-20 10:32:52 +0100

    Update OfferCard navigation => Details

--- frontend/src/OfferGallery/component/OfferCard.tsx
+++ frontend/src/OfferGallery/component/OfferCard.tsx
@@ -1,15 +1,22 @@
 import {Offer} from "../models/Offer";
-
+import {useNavigate} from "react-router-dom";
+import * as React from 'react';
 export type OfferCardProps = {
     offer: Offer
 }
 
 export default function OfferCard(props: OfferCardProps){
 
+    const navigate = useNavigate()
+
+    function handleDetailsClick(){
+        navigate("/details/" + props.offer.id)
+    }
+
     return(
         <div className={"offercard"}>
             {!props && <p>loading files...</p>}
-            <div className={"offercard_border"}>
+            <div className={"offercard_border"} onClick={handleDetailsClick}>
                 <h2>{props.offer.title}</h2>
                 <p>{props.offer.price}</p>
                 <hr/>
