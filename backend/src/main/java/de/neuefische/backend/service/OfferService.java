568b185c7b6dd2ca7e17840986ea8614b8d2cfb1
VladimirKlippert93 <vladimir.klippert93@gmail.com>
2022-12-20 11:40:08 +0100

    Update getOfferById

--- backend/src/main/java/de/neuefische/backend/service/OfferService.java
+++ backend/src/main/java/de/neuefische/backend/service/OfferService.java
@@ -1,10 +1,10 @@
 package de.neuefische.backend.service;
-
 import de.neuefische.backend.models.Offer;
 import de.neuefische.backend.repository.OfferRepo;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class OfferService {
@@ -16,5 +16,8 @@ public class OfferService {
 
     public List<Offer> getAllOffers(){
         return offerRepo.findAll();
+    }
+    public Optional<Offer> getOfferById(String id){
+        return offerRepo.findById(id);
     }
 }
