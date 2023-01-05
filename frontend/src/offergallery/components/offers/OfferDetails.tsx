5104f9eefd219426e09e4b42d0aa9a78478c7229
VladimirKlippert93 <vladimir.klippert93@gmail.com>
2023-01-05 16:56:35 +0100

    moved

--- frontend/src/offergallery/components/offers/OfferDetails.tsx
+++ frontend/src/offergallery/components/offers/OfferDetails.tsx
@@ -1,6 +1,6 @@
 import {useParams} from "react-router-dom";
 import {useEffect, useState} from "react";
-import {Offer} from "../../models/Offer";
+import {Offer} from "../models/Offer";
 import axios from "axios";
 
 export default function OfferDetails(){
