5c3171700205eff44d5921f5401734fa5fc3fd9b
VladimirKlippert93 <vladimir.klippert93@gmail.com>
2023-01-07 20:33:55 +0100

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,8 +3,12 @@ import './App.css';
 import OfferApp from "./offergallery/OfferApp";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import OfferDetails from "./offergallery/components/offers/OfferDetails";
+import useUser from "./offergallery/components/hooks/useUser";
+import LoginRegisterPage from "./offergallery/components/userpage/LoginRegisterPage";
 
 export default function App() {
+
+    const {login, register} = useUser();
 
   return (
     <div className="App">
@@ -13,6 +17,7 @@ export default function App() {
         <Routes>
             <Route path={"/offers/:id"} element={<OfferDetails/>}></Route>
             <Route path={"/"} element={<OfferApp/>}></Route>
+            <Route path={"/login"} element={<LoginRegisterPage login={login} register={register}/>}/>
         </Routes>
         </BrowserRouter>
     </div>
