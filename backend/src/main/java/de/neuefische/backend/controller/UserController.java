ceb83e03853ad15d9f4ca48711403114233e2a45
VladimirKlippert93 <vladimir.klippert93@gmail.com>
2023-01-12 10:47:39 +0100

    Add UserController

---
+++ backend/src/main/java/de/neuefische/backend/controller/UserController.java
@@ -0,0 +1,47 @@
+package de.neuefische.backend.controller;
+
+import de.neuefische.backend.models.MongoUser;
+import de.neuefische.backend.models.UserDTO;
+import de.neuefische.backend.service.UserService;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpSession;
+import java.security.Principal;
+
+@RestController
+@RequestMapping("api/users")
+public class UserController {
+
+    private final UserService userService;
+
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/me")
+    public String helloMe(Principal principal){
+        if(principal !=null){
+            return principal.getName();
+        }
+        return "unknownUser";
+    }
+
+    @PostMapping("/login")
+    public String login(){
+        return SecurityContextHolder.getContext().getAuthentication().getName();
+    }
+
+    @PostMapping("/logout")
+    public String logout(HttpSession httpSession){
+        httpSession.invalidate();
+        SecurityContextHolder.clearContext();
+        return "unkownUser";
+    }
+
+    @PostMapping("/register")
+    public MongoUser addUser (@RequestBody UserDTO userDTO){
+        return userService.addUser(userDTO);
+    }
+}
