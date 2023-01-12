a4bacb3ceed16010a7f4b44a6dbb25baef33e415
VladimirKlippert93 <vladimir.klippert93@gmail.com>
2023-01-12 12:08:47 +0100

    Add addUserTest

---
+++ backend/src/test/java/de/neuefische/backend/service/UserServiceTest.java
@@ -0,0 +1,43 @@
+package de.neuefische.backend.service;
+
+import de.neuefische.backend.models.UserDTO;
+import de.neuefische.backend.security.MongoUserRepo;
+import org.junit.jupiter.api.Test;
+
+import java.util.Collections;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+class UserServiceTest {
+
+    Argon2Service argon2Service = mock(Argon2Service.class);
+    MongoUserRepo mongoUserRepo = mock(MongoUserRepo.class);
+    UserService userService = new UserService(mongoUserRepo,argon2Service);
+
+    @Test
+    void addUser(){
+        UserDTO userToAdd = new UserDTO(
+
+                "username",
+                "123",
+                "email",
+                Collections.emptyList()
+        );
+        UserDTO expected = new UserDTO(
+
+                "username",
+                "****",
+                "email",
+                Collections.emptyList()
+        );
+
+        when(mongoUserRepo.save(expected)).thenReturn(expected);
+        when(argon2Service.encode("123")).thenReturn("****");
+
+        UserDTO actual = userService.addUser(userToAdd);
+
+        assertEquals(expected, actual);
+        verify(mongoUserRepo).save(expected);
+    }
+}
