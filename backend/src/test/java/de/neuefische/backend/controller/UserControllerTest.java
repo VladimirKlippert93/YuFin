5b4893e6cc143b366540fa53c0525c89745fa218
VladimirKlippert93 <vladimir.klippert93@gmail.com>
2023-01-12 12:40:05 +0100

    Add Login Test

--- backend/src/test/java/de/neuefische/backend/controller/UserControllerTest.java
+++ backend/src/test/java/de/neuefische/backend/controller/UserControllerTest.java
@@ -1,12 +1,14 @@
 package de.neuefische.backend.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import de.neuefische.backend.models.UserDTO;
 import de.neuefische.backend.security.MongoUserRepo;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
@@ -40,5 +42,19 @@ class UserControllerTest {
                 .andExpect(content().string("vladi"));
     }
 
-
+    @Test
+    @DirtiesContext
+    @WithMockUser
+    void login() throws Exception {
+        mockMvc.perform(post("/api/users/login").with(csrf())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                                {
+                                    "username": "user",
+                                    "password": "123"
+                                }
+                                """))
+                .andExpect(status().isOk())
+                .andExpect(content().string("user"));
+    }
 }
