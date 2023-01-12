ca5094ef2e2fb5db39ba9e1cb533dc26d6499e74
VladimirKlippert93 <vladimir.klippert93@gmail.com>
2023-01-12 10:07:15 +0100

    Rename User > MongoUser

--- backend/src/main/java/de/neuefische/backend/models/MongoUser.java
+++ backend/src/main/java/de/neuefische/backend/models/MongoUser.java
@@ -7,7 +7,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import java.util.List;
 
 @Document("User")
-public record User(
+public record MongoUser(
         @Id
         String id,
         String name,
