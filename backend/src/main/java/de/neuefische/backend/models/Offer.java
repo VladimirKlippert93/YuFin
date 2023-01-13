b22656fa44dab481d574080b6a0693d8eefda4f4
VladimirKlippert93 <vladimir.klippert93@gmail.com>
2023-01-13 12:06:42 +0100

    Delete id from offer

--- backend/src/main/java/de/neuefische/backend/models/Offer.java
+++ backend/src/main/java/de/neuefische/backend/models/Offer.java
@@ -1,12 +1,9 @@
 package de.neuefische.backend.models;
 
-import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document("Offer")
 public record Offer(
-        @Id
-        String id,
         String title,
         String price,
         Address address,
