db39cca37109b63da089b8ea403961792b6df174
VladimirKlippert93 <vladimir.klippert93@gmail.com>
2023-01-13 12:09:50 +0100

    Add addOffer Method

--- backend/src/main/java/de/neuefische/backend/controller/OfferController.java
+++ backend/src/main/java/de/neuefische/backend/controller/OfferController.java
@@ -3,10 +3,7 @@ package de.neuefische.backend.controller;
 import de.neuefische.backend.models.Offer;
 import de.neuefische.backend.service.OfferService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -28,5 +25,9 @@ public class OfferController {
     @GetMapping("/{id}")
     public Optional<Offer> getOfferById(@PathVariable String id){
         return offerService.getOfferById(id);
+    }
+    @PostMapping
+    public Offer addOffer(@RequestBody Offer offer){
+        return offerService.saveOffer(offer);
     }
 }
