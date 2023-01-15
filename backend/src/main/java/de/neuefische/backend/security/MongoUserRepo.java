51775f00c74c3051e5c82912138b61029fed63b0
VladimirKlippert93 <vladimir.klippert93@gmail.com>
2023-01-15 19:56:19 +0100

    Fixed Login/Register

--- backend/src/main/java/de/neuefische/backend/security/MongoUserRepo.java
+++ backend/src/main/java/de/neuefische/backend/security/MongoUserRepo.java
@@ -1,11 +1,10 @@
 package de.neuefische.backend.security;
 
 import de.neuefische.backend.models.MongoUser;
-import de.neuefische.backend.models.UserDTO;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.Optional;
 
-public interface MongoUserRepo extends MongoRepository<UserDTO, String> {
+public interface MongoUserRepo extends MongoRepository<MongoUser, String> {
     Optional<MongoUser> findByUsername(String name);
 }
