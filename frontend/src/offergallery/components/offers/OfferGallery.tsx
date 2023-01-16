25d23bea356fa957b3cdd67a5a1b318a6869deea
VladimirKlippert93 <vladimir.klippert93@gmail.com>
2023-01-16 19:07:57 +0100

    Update props => deleteOffer

--- frontend/src/offergallery/components/offers/OfferGallery.tsx
+++ frontend/src/offergallery/components/offers/OfferGallery.tsx
@@ -1,14 +1,17 @@
 import {Offer} from "../models/Offer";
 import OfferCard from "./OfferCard";
+import {User} from "../models/User";
 
 type OfferGalleryProps = {
-    offerList : Offer[]
+    offerList : Offer[],
+    user: User
+    deleteOffer: (id: string | undefined) => void
 }
 
 export default function OfferGallery(props: OfferGalleryProps){
 
     const offerComponents = props.offerList.map((offer)=>{
-        return <OfferCard offer={offer} key={offer._id}/>
+        return <OfferCard offer={offer} key={offer._id} user={props.user} deleteOffer={props.deleteOffer}/>
     })
 
     return(
