6eca133649dbfb014d18caa1ccae070d22510e0a
VladimirKlippert93 <vladimir.klippert93@gmail.com>
2023-01-16 14:20:57 +0100

    Add user props & show Username

--- frontend/src/offergallery/OfferApp.tsx
+++ frontend/src/offergallery/OfferApp.tsx
@@ -3,8 +3,12 @@ import {useEffect, useState} from "react";
 import {Offer} from "./components/models/Offer";
 import axios from "axios";
 import SearchBar from "./components/offers/SearchBar";
+import {User} from "./components/models/User";
 
-export default function OfferApp(){
+type OfferAppProps={
+    user: User
+}
+export default function OfferApp(props: OfferAppProps){
 
     const [offers, setOffers] = useState<Offer[]>([])
     const [searchText, setSearchText] = useState<string>("")
@@ -29,6 +33,7 @@ export default function OfferApp(){
 
     return (
         <div>
+            <h1>{props.user.username}</h1>
             <SearchBar handleSearchText={handleSearchText}/>
             <OfferGallery offerList={filteredSearch}/>
         </div>
