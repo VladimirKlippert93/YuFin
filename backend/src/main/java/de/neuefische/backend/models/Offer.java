0c198adf6d151ef957e56db9c92d1feb305a9c33
VladimirKlippert93 <vladimir.klippert93@gmail.com>
2023-01-16 14:09:43 +0100

    Update Offer.java with Id

--- backend/src/main/java/de/neuefische/backend/models/Offer.java
+++ backend/src/main/java/de/neuefische/backend/models/Offer.java
@@ -1,9 +1,12 @@
 package de.neuefische.backend.models;
 
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document("Offer")
 public record Offer(
+        @Id
+        String _id,
         String title,
         String price,
         Address address,
