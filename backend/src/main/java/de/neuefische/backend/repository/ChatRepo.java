98c7cde152090430e9e53f3bfd86d3184ce80e0e
VladimirKlippert93 <vladimir.klippert93@gmail.com>
2023-01-19 15:20:59 +0100

    Add ChatRepo

---
+++ backend/src/main/java/de/neuefische/backend/repository/ChatRepo.java
@@ -0,0 +1,10 @@
+package de.neuefische.backend.repository;
+
+import de.neuefische.backend.models.ChatMessage;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+public interface ChatRepo extends MongoRepository<ChatMessage, String> {
+    List<ChatMessage> findAllBySenderUsernameAndReceiverUsername(String senderUsername, String receiverUsername);
+}
