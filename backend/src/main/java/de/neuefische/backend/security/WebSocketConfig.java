3b2d3b28c5a72795880300a8bcf0917951192f18
VladimirKlippert93 <vladimir.klippert93@gmail.com>
2023-01-19 15:23:08 +0100

    Add WebSocketConfig

---
+++ backend/src/main/java/de/neuefische/backend/security/WebSocketConfig.java
@@ -0,0 +1,23 @@
+package de.neuefische.backend.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.*;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws/{senderUsername}/{receiverUsername}")
+                .setAllowedOrigins("*")
+                .withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.setApplicationDestinationPrefixes("/app")
+                .enableSimpleBroker("/topic");
+    }
+}
