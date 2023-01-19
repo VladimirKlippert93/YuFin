af63cb2c9432be436da0473a924e181d6f6c7c62
VladimirKlippert93 <vladimir.klippert93@gmail.com>
2023-01-19 15:25:19 +0100

    Update App with the ChatPage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,6 +6,9 @@ import OfferDetails from "./offergallery/components/offers/OfferDetails";
 import useUser from "./offergallery/components/hooks/useUser";
 import LoginRegisterPage from "./offergallery/components/userpage/LoginRegisterPage";
 import AddOffer from "./offergallery/components/offers/AddOffer";
+import ChatPage from "./offergallery/components/Chat/ChatPage";
+
+
 
 export default function App() {
 
@@ -20,6 +23,8 @@ export default function App() {
             <Route path={"/addoffer"} element={<AddOffer user={user}/>}></Route>
             <Route path={"/"} element={<OfferApp user={user}/>}></Route>
             <Route path={"/login"} element={<LoginRegisterPage login={login} register={register}/>}/>
+            <Route path={"/chat/:senderUsername/:receiverUsername"} element={<ChatPage senderUsername={user.username} receiverUsername={user.username}/>}/>
+
         </Routes>
         </BrowserRouter>
     </div>
