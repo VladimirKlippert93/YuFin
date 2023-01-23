cc5714f86dcfc21721d276fa2ff6e76cc9dba379
VladimirKlippert93 <vladimir.klippert93@gmail.com>
2023-01-23 11:29:14 +0100

    Update repo

--- backend/src/main/java/de/neuefische/backend/repository/ChatRepo.java
+++ backend/src/main/java/de/neuefische/backend/repository/ChatRepo.java
@@ -6,7 +6,8 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import java.util.List;
 
 public interface ChatRepo extends MongoRepository<ChatMessage, String> {
-    List<ChatMessage> findAllBySenderUsernameOrReceiverUsername(String senderUsername, String receiverUsername);
 
     ChatMessage findFirstBySenderUsernameOrderByTimestampDesc(String senderUsername);
+
+    List<ChatMessage> findAllBySenderUsernameAndReceiverUsername(String senderUsername, String receiverUsername);
 }
