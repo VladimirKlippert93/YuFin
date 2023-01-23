8f7da18a2cb1388fd82ba1629f3b1b998e6ce6b3
VladimirKlippert93 <vladimir.klippert93@gmail.com>
2023-01-23 12:58:56 +0100

    Update Test ->handleTextMessage_shouldSaveMessageInRepo, handleTextMessage_shouldSendMessageToReceiver, handleTextMessage_shouldSetId

--- backend/src/test/java/de/neuefische/backend/service/ChatServiceTest.java
+++ backend/src/test/java/de/neuefische/backend/service/ChatServiceTest.java
@@ -3,16 +3,17 @@ package de.neuefische.backend.service;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import de.neuefische.backend.models.ChatMessage;
 import de.neuefische.backend.repository.ChatRepo;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 
-import java.security.Principal;
 import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.List;
@@ -31,6 +32,13 @@ class ChatServiceTest {
     private WebSocketSession session;
     @InjectMocks
     private ChatService chatService;
+    @Captor
+    private ArgumentCaptor<TextMessage> textMessageCaptor;
+
+    @BeforeEach
+    public void setUp() {
+        chatService = new ChatService(chatRepo, objectMapper);
+    }
 
     @Test
     void saveMessageTest() {
@@ -45,29 +53,69 @@ class ChatServiceTest {
 
     @Test
     void sendPreviousMessagesTest() throws Exception {
-            List<ChatMessage> messages = Arrays.asList(
-                    new ChatMessage("sender", "receiver", "Hello", LocalDateTime.now()),
-                    new ChatMessage("receiver", "sender", "Hi", LocalDateTime.now())
-            );
-            lenient().when(session.getPrincipal()).thenReturn(new Principal() {
-                @Override
-                public String getName() {
-                    return "sender";
-                }
-            });
-            when(objectMapper.writeValueAsString(messages.get(0))).thenReturn("{\"sender\":\"sender\",\"receiver\":\"receiver\",\"message\":\"Hello\",\"timestamp\":\""+messages.get(0).getTimestamp()+"\"}");
-            when(objectMapper.writeValueAsString(messages.get(1))).thenReturn("{\"sender\":\"receiver\",\"receiver\":\"sender\",\"message\":\"Hi\",\"timestamp\":\""+messages.get(1).getTimestamp()+"\"}");
-            when(chatRepo.findAllBySenderUsernameAndReceiverUsername("sender", "receiver"))
-                    .thenReturn(messages);
-            chatService.sendPreviousMessages(session, "sender", "receiver");
-            ArgumentCaptor<TextMessage> textMessageArgumentCaptor = ArgumentCaptor.forClass(TextMessage.class);
-            verify(session, times(2)).sendMessage(textMessageArgumentCaptor.capture());
-            List<TextMessage> capturedTextMessages = textMessageArgumentCaptor.getAllValues();
+        List<ChatMessage> messages = Arrays.asList(
+                new ChatMessage("sender", "receiver", "Hello", LocalDateTime.now()),
+                new ChatMessage("receiver", "sender", "Hi", LocalDateTime.now())
+        );
+        lenient().when(session.getPrincipal()).thenReturn(() -> "sender");
+        when(objectMapper.writeValueAsString(messages.get(0))).thenReturn("{\"sender\":\"sender\",\"receiver\":\"receiver\",\"message\":\"Hello\",\"timestamp\":\"" + messages.get(0).getTimestamp() + "\"}");
+        when(objectMapper.writeValueAsString(messages.get(1))).thenReturn("{\"sender\":\"receiver\",\"receiver\":\"sender\",\"message\":\"Hi\",\"timestamp\":\"" + messages.get(1).getTimestamp() + "\"}");
+        when(chatRepo.findAllBySenderUsernameAndReceiverUsername("sender", "receiver"))
+                .thenReturn(messages);
+        chatService.sendPreviousMessages(session, "sender", "receiver");
+        ArgumentCaptor<TextMessage> textMessageArgumentCaptor = ArgumentCaptor.forClass(TextMessage.class);
+        verify(session, times(2)).sendMessage(textMessageArgumentCaptor.capture());
+        List<TextMessage> capturedTextMessages = textMessageArgumentCaptor.getAllValues();
 
-            assertEquals("{\"sender\":\"sender\",\"receiver\":\"receiver\",\"message\":\"Hello\",\"timestamp\":\""+messages.get(0).getTimestamp()+"\"}", capturedTextMessages.get(0).getPayload());
-            assertEquals("{\"sender\":\"receiver\",\"receiver\":\"sender\",\"message\":\"Hi\",\"timestamp\":\""+messages.get(1).getTimestamp()+"\"}", capturedTextMessages.get(1).getPayload());
-            verify(chatRepo).findAllBySenderUsernameAndReceiverUsername("sender", "receiver");
-            verify(chatRepo).findAllBySenderUsernameAndReceiverUsername("receiver", "sender");
-            verifyNoMoreInteractions(session, chatRepo, objectMapper);
+        assertEquals("{\"sender\":\"sender\",\"receiver\":\"receiver\",\"message\":\"Hello\",\"timestamp\":\"" + messages.get(0).getTimestamp() + "\"}", capturedTextMessages.get(0).getPayload());
+        assertEquals("{\"sender\":\"receiver\",\"receiver\":\"sender\",\"message\":\"Hi\",\"timestamp\":\"" + messages.get(1).getTimestamp() + "\"}", capturedTextMessages.get(1).getPayload());
+        verify(chatRepo).findAllBySenderUsernameAndReceiverUsername("sender", "receiver");
+        verify(chatRepo).findAllBySenderUsernameAndReceiverUsername("receiver", "sender");
+        verifyNoMoreInteractions(session, chatRepo, objectMapper);
+    }
+
+    @Test
+    void handleTextMessage_shouldSaveMessageInRepo() throws Exception {
+        // given
+        ChatMessage chatMessage = new ChatMessage("sender", "receiver", "Hello", LocalDateTime.now());
+        when(objectMapper.readValue(anyString(), eq(ChatMessage.class))).thenReturn(chatMessage);
+        when(session.getPrincipal()).thenReturn(() -> "sender");
+        when(objectMapper.writeValueAsString(chatMessage)).thenReturn("{\"senderUsername\":\"sender\",\"receiverUsername\":\"receiver\",\"message\":\"Hello\",\"timestamp\":\"2022-01-23T10:15:30\"}");
+
+        // when
+        chatService.handleTextMessage(session, new TextMessage("{\"senderUsername\":\"sender\",\"receiverUsername\":\"receiver\",\"message\":\"Hello\",\"timestamp\":\"2022-01-23T10:15:30\"}"));
+
+        // then
+        verify(chatRepo, times(1)).save(chatMessage);
+    }
+
+    @Test
+    void handleTextMessage_shouldSendMessageToReceiver() throws Exception {
+        // given
+        ChatMessage chatMessage = new ChatMessage("sender", "receiver", "Hello", LocalDateTime.now());
+        when(objectMapper.readValue(anyString(), eq(ChatMessage.class))).thenReturn(chatMessage);
+        when(session.getPrincipal()).thenReturn(() -> "sender");
+        when(objectMapper.writeValueAsString(chatMessage)).thenReturn("{\"senderUsername\":\"sender\",\"receiverUsername\":\"receiver\",\"message\":\"Hello\",\"timestamp\":\"2022-01-23T10:15:30\"}");
+        // when
+        chatService.handleTextMessage(session, new TextMessage("{\"senderUsername\":\"sender\",\"receiverUsername\":\"receiver\",\"message\":\"Hello\",\"timestamp\":\"2022-01-23T10:15:30\"}"));
+
+        // then
+        verify(session, times(1)).sendMessage(textMessageCaptor.capture());
+        TextMessage sentTextMessage = textMessageCaptor.getValue();
+        assertEquals("{\"senderUsername\":\"sender\",\"receiverUsername\":\"receiver\",\"message\":\"Hello\",\"timestamp\":\"2022-01-23T10:15:30\"}", sentTextMessage.getPayload());
+    }
+
+    @Test
+    void handleTextMessage_shouldSetId() throws Exception {
+        // given
+        ChatMessage chatMessage = new ChatMessage("sender", "receiver", "Hello", LocalDateTime.now());
+        when(objectMapper.readValue(anyString(), eq(ChatMessage.class))).thenReturn(chatMessage);
+        when(session.getPrincipal()).thenReturn(() -> "sender");
+        when(objectMapper.writeValueAsString(chatMessage)).thenReturn("{\"senderUsername\":\"sender\",\"receiverUsername\":\"receiver\",\"message\":\"Hello\",\"timestamp\":\"2022-01-23T10:15:30\"}");
+        // when
+        chatService.handleTextMessage(session, new TextMessage("{\"senderUsername\":\"sender\",\"receiverUsername\":\"receiver\",\"message\":\"Hello\",\"timestamp\":\"2022-01-23T10:15:30\"}"));
+
+        // then
+        assertEquals(String.class, chatMessage.getId().getClass());
     }
 }
