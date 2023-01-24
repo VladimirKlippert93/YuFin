11a13e233e5f152d2f12a414f7edfcaeccca6eb1
VladimirKlippert93 <vladimir.klippert93@gmail.com>
2023-01-24 12:05:03 +0100

    Update ChatMessage to lombok

--- backend/src/main/java/de/neuefische/backend/models/ChatMessage.java
+++ backend/src/main/java/de/neuefische/backend/models/ChatMessage.java
@@ -1,10 +1,18 @@
 package de.neuefische.backend.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 @Document("Message")
 public class ChatMessage {
     @Id
@@ -14,34 +22,5 @@ public class ChatMessage {
     private String message;
     private LocalDateTime timestamp;
 
-    public ChatMessage(String senderUsername, String receiverUsername, String message, LocalDateTime timestamp) {
-        this.senderUsername = senderUsername;
-        this.receiverUsername = receiverUsername;
-        this.message = message;
-        this.timestamp = timestamp;
-    }
 
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public void setSenderUsername(String senderUsername) {
-        this.senderUsername = senderUsername;
-    }
-
-    public String getReceiverUsername() {
-        return receiverUsername;
-    }
-
-    public LocalDateTime getTimestamp() {
-        return timestamp;
-    }
-
-    public void setTimestamp(LocalDateTime timestamp) {
-        this.timestamp = timestamp;
-    }
 }
