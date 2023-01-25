8dc36eb44ba3c2a7f9c84efe030134e28f507016
VladimirKlippert93 <vladimir.klippert93@gmail.com>
2023-01-25 12:31:36 +0100

    Update the App Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,25 +7,28 @@ import useUser from "./offergallery/components/hooks/useUser";
 import LoginRegisterPage from "./offergallery/components/userpage/LoginRegisterPage";
 import AddOffer from "./offergallery/components/offers/AddOffer";
 import ChatPage from "./offergallery/components/Chat/ChatPage";
-
+import Header from "./offergallery/components/Header";
+import ChatOverview from "./offergallery/components/Chat/ChatOverview";
 
 
 export default function App() {
 
     const {user, login, register} = useUser();
 
-  return (
-    <div className="App">
+    return (
+        <div className="App">
 
-        <BrowserRouter>
-        <Routes>
-            <Route path={"/offers/:id"} element={<OfferDetails/>}></Route>
-            <Route path={"/addoffer"} element={<AddOffer user={user}/>}></Route>
-            <Route path={"/"} element={<OfferApp user={user}/>}></Route>
-            <Route path={"/login"} element={<LoginRegisterPage login={login} register={register}/>}/>
-            <Route path="/chat/:receiverUsername" element={<ChatPage/>} />
-        </Routes>
-        </BrowserRouter>
-    </div>
-  );
+            <BrowserRouter>
+                <Header  user={user}/>
+                <Routes>
+                    <Route path={"/"} element={<OfferApp user={user}/>}></Route>
+                    <Route path={"/addoffer"} element={<AddOffer user={user}/>}></Route>
+                    <Route path={"/login"} element={<LoginRegisterPage login={login} register={register}/>}/>
+                    <Route path={"/offers/:id"} element={<OfferDetails/>}></Route>
+                    <Route path="/chat/:receiverUsername" element={<ChatPage/>}/>
+                    <Route path={"/chatoverview"} element={<ChatOverview/>} />
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
