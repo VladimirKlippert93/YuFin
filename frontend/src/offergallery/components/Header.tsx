cb0435464f6367e2b68c3c75bd16b419a39e6f33
VladimirKlippert93 <vladimir.klippert93@gmail.com>
2023-01-25 15:04:59 +0100

    Update Header -> logout props and added classnames

--- frontend/src/offergallery/components/Header.tsx
+++ frontend/src/offergallery/components/Header.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import Navbar from './Navbar';
 import {User} from "./models/User";
+import "../../styles/Header.css";
 
 type HeaderProps={
     user:User
+    logout: ()=>void
 }
 
 export default function Header(props: HeaderProps){
     return (
-        <header>
-            <h1>YuFin</h1>
-            <p>You need it? You find it!</p>
-            <Navbar  user={props.user}/>
+        <header className="header">
+            <h1 className="header_title">YuFin</h1>
+            <p className="header_subtitle">You need it? You find it!</p>
+            <Navbar  user={props.user} logout={props.logout}/>
         </header>
     );
 }
