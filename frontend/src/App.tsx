9bffc6b5f654f26da9050d63901c3360c7b45406
VladimirKlippert93 <vladimir.klippert93@gmail.com>
2023-01-25 15:06:34 +0100

    Update logout from useUser

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,24 @@
 import React from 'react';
-import './App.css';
 import OfferApp from "./offergallery/OfferApp";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import OfferDetails from "./offergallery/components/offers/OfferDetails";
 import useUser from "./offergallery/components/hooks/useUser";
 import LoginRegisterPage from "./offergallery/components/userpage/LoginRegisterPage";
 import AddOffer from "./offergallery/components/offers/AddOffer";
-import ChatPage from "./offergallery/components/Chat/ChatPage";
+import ChatPage from "./offergallery/components/chat/ChatPage";
 import Header from "./offergallery/components/Header";
-import ChatOverview from "./offergallery/components/Chat/ChatOverview";
+import ChatOverview from "./offergallery/components/chat/ChatOverview";
 
 
 export default function App() {
 
-    const {user, login, register} = useUser();
+    const {user, login, logout, register} = useUser();
 
     return (
         <div className="App">
 
             <BrowserRouter>
-                <Header  user={user}/>
+                <Header  user={user} logout={logout}/>
                 <Routes>
                     <Route path={"/"} element={<OfferApp user={user}/>}></Route>
                     <Route path={"/addoffer"} element={<AddOffer user={user}/>}></Route>
