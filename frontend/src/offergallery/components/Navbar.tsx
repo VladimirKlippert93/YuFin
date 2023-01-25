6397e8d8a56ea84337d4443f3b1d0ffa10ec19b7
VladimirKlippert93 <vladimir.klippert93@gmail.com>
2023-01-25 15:04:19 +0100

    Update Navbar so that the elements will change if a user is logged in or not

--- frontend/src/offergallery/components/Navbar.tsx
+++ frontend/src/offergallery/components/Navbar.tsx
@@ -1,28 +1,34 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import {User} from "./models/User";
+import "../../styles/Navbar.css"
 
 type NavbarProps = {
     user: User
+    logout: ()=>void
 }
 
 export default function Navbar(props: NavbarProps) {
 
     return (
-        <nav>
-            <ul>
-                <li>
-                    <Link to="/">Home</Link>
+        <nav className="navbar">
+            <ul className="navbar_list">
+                <li className="navbar_item">
+                    <Link to="/" className="navbar_link">Home</Link>
                 </li>
-                {props.user.username !== "" && <li>
-                    <Link to="/addoffer">Add Offer</Link>
+                {props.user.username !== "" && <li className="navbar_item">
+                    <Link to="/addoffer" className="navbar_link">AddOffer</Link>
                 </li>}
-                {props.user.username !== "" && <li>
-                    <Link to="/chatoverview">ChatOverview</Link>
-                </li>}
-                <li>
-                    <Link to="/login">Login/Register</Link>
+                {props.user.username !== "" && <li className="navbar_item">
+                    <Link to="/chatoverview" className="navbar_link">Chats</Link>
                 </li>
+                }{props.user.username === "" && <li className="navbar_item">
+                <Link to="/login" className="navbar_link">Login/Register</Link>
+            </li>}
+                {props.user.username !== "" && <li className="navbar_item">
+                    <Link to="#" className="navbar_link" onClick={() => props.logout()}>{props.user.username} Logout</Link>
+                </li>}
+
             </ul>
         </nav>
     );
